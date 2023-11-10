dcdef5e8a8981117e7dec3d2c233495fe8a173b2
Margarita Shishkova <margolen239@gmail.com>
2023-11-10 21:04:24 +0000

    Create mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuClose = document.querySelector(".menu__close");
 const menuList = document.querySelector(".menu__list");
-const menuShadow = document.querySelector(".menu__close");
+const menuShadow = document.querySelector(".menu--close");
 
 menuBtn.addEventListener("click", () => {
   menuList.classList.toggle("menu__list--open");
